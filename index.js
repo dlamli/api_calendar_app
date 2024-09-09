f4a5e5e4615e71b752a18dbf1c0b49c5c8d1c272
dlamli <dannylamli@hotmail.com>
2024-09-08 21:28:16 -0600

    [feat]Update React App

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 const authRoutes = require("./routes/auth.routes");
 const eventRoutes = require("./routes/event.routes");
@@ -21,6 +22,10 @@ app.use(express.json());
 // API ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/event", eventRoutes);
+
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`SERVER LISTENING ON PORT: ${process.env.PORT}`);
